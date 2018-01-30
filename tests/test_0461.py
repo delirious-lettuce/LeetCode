ad8b33fdbeb731c888d94a485afdc7d745933952
delirious-lettuce <delirious.lettuce@gmail.com>
2018-01-30 10:27:51 -0700

    Adjust formatting

--- tests/test_0461.py
+++ tests/test_0461.py
@@ -8,13 +8,10 @@ def solution():
     return problem_0461.Solution()
 
 
-@pytest.mark.parametrize(
-    'test_input, expected',
-    (
-        ((1, 4), 2),
-        ((142, 77), 4),
-        ((13435, 98237), 9)
-    )
-)
+@pytest.mark.parametrize('test_input, expected', (
+    ((1, 4), 2),
+    ((142, 77), 4),
+    ((13435, 98237), 9)
+))
 def test_hamming_distance(test_input, expected, solution):
     assert solution.hammingDistance(*test_input) == expected
